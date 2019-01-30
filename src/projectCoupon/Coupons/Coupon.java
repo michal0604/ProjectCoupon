4bc223c6bd016a4ab4c32d41ec07cefe71201c72
michal0604 <mn116@walla.co.il>
2019-01-30 22:23:32 +0200

    insert coupon table

---
+++ src/projectCoupon/Coupons/Coupon.java
@@ -0,0 +1,79 @@
+package projectCoupon.Coupons;
+
+import java.sql.Date;
+
+public class Coupon {
+	private long ID;
+	private String Title;
+	private Date Start_date;
+	private Date End_Date;
+	private Integer amount;
+	 public enum string {
+		 type(); 
+	 }
+	private String Message;
+	private Double Price;
+	private String image;
+	
+	public Coupon() {
+
+	}
+	
+	public Coupon(long iD, String title, Date start_date, Date end_Date, Integer amount, String message, Double price,
+			String image) {
+		super();
+		ID = iD;
+		Title = title;
+		Start_date = start_date;
+		End_Date = end_Date;
+		this.amount = amount;
+		Message = message;
+		Price = price;
+		this.image = image;
+	}
+	public String getTitle() {
+		return Title;
+	}
+	public void setTitle(String title) {
+		Title = title;
+	}
+	public Date getStart_date() {
+		return Start_date;
+	}
+	public void setStart_date(Date start_date) {
+		Start_date = start_date;
+	}
+	public Date getEnd_Date() {
+		return End_Date;
+	}
+	public void setEnd_Date(Date end_Date) {
+		End_Date = end_Date;
+	}
+	public Integer getAmount() {
+		return amount;
+	}
+	public void setAmount(Integer amount) {
+		this.amount = amount;
+	}
+	public String getMessage() {
+		return Message;
+	}
+	public void setMessage(String message) {
+		Message = message;
+	}
+	public Double getPrice() {
+		return Price;
+	}
+	public void setPrice(Double price) {
+		Price = price;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+	
+
+}
---
+++ src/projectCoupon/Coupons/CouponDBDAO.java
@@ -0,0 +1,71 @@
+package projectCoupon.Coupons;
+
+	import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Set;
+
+import projectCoupon.Database;
+
+	public class CouponDBDAO implements CouponDAO {
+		Connection con;
+
+		@Override
+		public void insertCoupon(Coupon Coupon) throws Exception {
+			con = DriverManager.getConnection(Database.getDBUrl());
+			//createTables(con);
+			String sql = "INSERT INTO Coupon (Title,Start_date,End_Date,amount,type()) VALUES(?,?,?,?,?)";
+
+			try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+				pstmt.setString(1, Coupon.getTitle());
+				pstmt.setDate(2, Coupon.getStart_date());
+				pstmt.setDate(2, Coupon.getEnd_Date());
+				pstmt.setInt(4, Coupon.getAmount());
+				pstmt.executeUpdate();
+
+				// why 1 2 3
+
+				System.out.println("Company created" + Company.toString());
+			} catch (SQLException ex) {
+				System.out.println(ex.getLocalizedMessage());
+				throw new Exception("Company creation failed");
+			} finally {
+				con.close();
+			}
+		}
+			
+		}
+
+		@Override
+		public void removeCoupon(Coupon Coupon) throws Exception {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void updateCoupon(Coupon Coupon) throws Exception {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public Coupon getPCoupon(long id) throws Exception {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Set<Coupon> getAllCoupon() throws Exception {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Coupon dropTable() throws Exception {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+	}
