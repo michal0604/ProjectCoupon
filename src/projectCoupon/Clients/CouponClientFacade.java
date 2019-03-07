2c4f8f3ba7011a7c6c19a2a1591aa6fd6b1a455f
EivyFridman <45360415+EivyFridman@users.noreply.github.com>
2019-03-07 19:15:05 +0200

    revert to file

---
+++ src/projectCoupon/Clients/CouponClientFacade.java
@@ -0,0 +1,17 @@
+package projectCoupon.Clients;
+
+public interface CouponClientFacade {
+	
+//	public static String user = null;
+//	public static String password = null;
+//	public static ClientType type = null;
+	
+	
+
+	CouponClientFacade login(String name, String password) throws Exception;
+
+	
+	
+}
+
+
